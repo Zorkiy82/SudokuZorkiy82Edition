faf67cfbca0a612804a009c23ea872fa4ce0bab3
Aleksandr Ogorodnikov <Ogorodnikov.Aaleksandr@yandex.ru>
2022-07-18 12:37:41 +0500

    +selection

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,45 @@
 import { Cell } from "../scripts/components/Cell.js";
 
-const initialArray =[
-  [0,3,1,6,0,7,0,0,0],
-  [6,0,0,8,0,0,2,5,7],
-  [8,0,0,0,9,0,6,0,3],
-  [4,0,0,0,0,0,8,3,2],
-  [0,1,0,0,6,9,0,0,0],
-  [7,0,3,2,4,0,0,0,6],
-  [9,0,2,4,0,1,0,7,8],
-  [0,8,5,0,0,0,0,0,9],
-  [3,0,4,0,0,0,0,6,1],
+const initialArray = [
+  [0, 3, 1, 6, 0, 7, 0, 0, 0],
+  [6, 0, 0, 8, 0, 0, 2, 5, 7],
+  [8, 0, 0, 0, 9, 0, 6, 0, 3],
+  [4, 0, 0, 0, 0, 0, 8, 3, 2],
+  [0, 1, 0, 0, 6, 9, 0, 0, 0],
+  [7, 0, 3, 2, 4, 0, 0, 0, 6],
+  [9, 0, 2, 4, 0, 1, 0, 7, 8],
+  [0, 8, 5, 0, 0, 0, 0, 0, 9],
+  [3, 0, 4, 0, 0, 0, 0, 6, 1],
 ];
 
-let cellsArray = [[],[],[],[],[],[],[],[],[]]
+let cellsArray = [[], [], [], [], [], [], [], [], []];
 
-let cellsList = document.querySelectorAll('.field__cell');
+let cellsList = document.querySelectorAll(".field__cell");
 let counter = 0;
-for (let x=0; x<9; ++x){
-  for (let y=0; y<9; ++y){
+
+let selectedCell = null;
+
+function hadleMouseDownToCell(curentCell) {
+  if (selectedCell) {
+    selectedCell.toggleSelected();
+  }
+
+  if (selectedCell === curentCell) {
+    selectedCell = null;
+  } else {
+    selectedCell = curentCell;
+    selectedCell.toggleSelected();
+  }
+}
+
+for (let x = 0; x < 9; ++x) {
+  for (let y = 0; y < 9; ++y) {
     // debugger;
-    cellsArray[x][y]= new Cell(cellsList[counter],initialArray[x][y]);
+    cellsArray[x][y] = new Cell(
+      cellsList[counter],
+      initialArray[x][y],
+      hadleMouseDownToCell
+    );
     cellsArray[x][y].initialRender();
     ++counter;
   }
--- scripts/components/Cell.js
+++ scripts/components/Cell.js
@@ -1,14 +1,17 @@
 class Cell{
-  constructor(cellElement, initValue){
+  constructor(cellElement, initValue, hadleMouseDown){
     // debugger;
     this._cellElement = cellElement;
     this._cellMainDigitElement = this._cellElement.querySelector('.field__cell-main-digit');
     this._cellPossibleDigitsElementsList = this._cellElement.querySelector('.field__cell_list').querySelectorAll('li');
     this._initValue = initValue;
     this._isStaticCell = !(initValue === 0);
+    this._hadleMouseDown = hadleMouseDown;
   }
 
   initialRender(){
+    this._setListeners();
+
     if (this._isStaticCell) {
         this._cellMainDigitElement.textContent = String(this._initValue);
     } else {
@@ -21,6 +24,14 @@ class Cell{
     possibleDigitsArray.forEach((item, index) => {
       this._cellPossibleDigitsElementsList[index].textContent =  String(item);
     });
+  }
+
+  _setListeners(){
+    this._cellElement.addEventListener('mousedown',()=>this._hadleMouseDown(this));
+  }
+
+  toggleSelected(){
+    this._cellElement.classList.toggle('field__cell_selected');
   }
 }
 
